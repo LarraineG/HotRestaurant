fae17d2ae9195897b52e9b74834afde130a81486
Larraine Gonzales <larrainegonzales@hotmail.com>
2020-09-01 16:24:28 -0500

    updated reservations post call

--- server.js
+++ server.js
@@ -1,55 +1,82 @@
-// Create Global Variables
+
 var express = require("express");
 var path = require("path");
 
-
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
-
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Create Reservations
 
-  const reservations = [
+const reservations = [
     {
-        "name": "#"
-        , "id": "11111"
-        , "phone": "123-555-1234"
-        , "email": "poop@gmail.com"
+        "name": "Homer Simpson"
+        , "id": "0000"
+        , "phone": "555-555-1234"
+        , "email": "Mmmmm@donuts.com"
     },
     {
-        "name": "#"
-        , "id": "22222"
-        , "phone": "281-555-4567"
-        , "email": "yep@gmail.com"
+        "name": "Norman Bates"
+        , "id": "1959"
+        , "phone": "555-555-2345"
+        , "email": "ilovemymom@batesmotel.com"
     },
-]
-// Create Tables
-// Front-End Routes
-app.get("/", function(req, res) {
+    {
+        "name": "Dr. Emmett Brown"
+        , "id": "1555"
+        , "phone": "555-555-3456"
+        , "email": "greatscott@thefuture.com"
+    },
+];
+
+const tables = [
+    {
+        "name": "Lucy Ricardo"
+        , "id": "1951"
+        , "phone": "555-555-4567"
+        , "email": "vitameatavegamin@waaaah.com"
+    },
+];
+
+// FRONT-END ROUTES_______________________________________
+
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
-  });
+});
 
-app.get("/reserve", function(req, res) {
+app.get("/reserve", function (req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
-  });
+});
 
-app.get("/tables", function(req, res) {
+app.get("/tables", function (req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
-  });
-// API Routes
-app.get("/api/reservation", function (req, res) {
-    res.json(reservation);
+});
+
+// API ROUTES_____________________________________________
+app.get("/api/reservations", function (req, res) {
+    res.json(reservations);
 });
 
 app.get("/api/tables", function (req, res) {
     res.json(tables);
 });
 
-// POST Call
+// POST CALL______________________________________________
+app.post("/api/reservations", (req, res) => {
+    const newReservation = req.body;
+    if (tables.length <= 4) {
+        tables.push(newReservation);
+        res.json(newReservation);
+    } else {
+        reservations.push(newReservation);
+        res.json(newReservation);
+    }
+})
 
-// Server Listen
-app.listen(PORT, function() {
+
+
+// SERVER LISTEN_________________________________________
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-  });
+});
